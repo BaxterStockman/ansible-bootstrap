21572143d9bed20878c5e09fbfefaa8d2afc9e53
Matt Schreiber <EMAIL>
2015-10-07 14:55:49 -0400

    Corrected fatal error in action_plugin due to missing remote tmp directory.  Problem was that ansible was deleting the remote tmp directory after the 'copy' calls, so the passthru module had nowhere to upload its module script :(

--- action_plugins/bootstrap.py
+++ action_plugins/bootstrap.py
@@ -18,61 +18,140 @@
 
 import os
 
-from ansible.module_utils.basic import *
+import ansible.constants as C
+
 from ansible import utils
 from ansible import errors
 from ansible.runner.return_data import ReturnData
+
+
+class tmp_keep_remote_files(object):
+    def __init__(self, tmp_value):
+        self.tmp_value = tmp_value
+
+    def __enter__(self):
+        self.stored_value = C.DEFAULT_KEEP_REMOTE_FILES
+        return self.tmp_value
+
+    def __exit__(self, type, value, traceback):
+        C.DEFAULT_KEEP_REMOTE_FILES = self.stored_value
+
 
 class ActionModule(object):
 
     TRANSFERS_FILES = True
 
+    copy_module_key = 'sources'
+
     def __init__(self, runner):
         self.runner = runner
+        # Instantiate the action_plugin for the 'copy' module
+        self.copy_handler = utils.plugins.action_loader.get('copy', runner)
+
+    def _copy(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
+        return self.copy_handler.run(conn, tmp, 'copy', module_args, inject,
+                                     complex_args=complex_args, **kwargs)
+
+    def _partition_options(self, options=None, key=copy_module_key):
+        if not options:
+            return ({}, {})
+
+        extracted = options.get(key, {})
+        rest = {k:v for k, v in options.iteritems() if not k == key}
+
+        return extracted, rest
+
+    def _make_sources_map(self, sources_list=None):
+        sources_map = {}
+
+        if sources_list:
+            try:
+                for item in sources_list:
+                    if isinstance(item, basestring):
+                        item = utils.parse_kv(item)
+                    src = item['src']
+                    sources_map[src] = item
+            except KeyError:
+                raise errors.AnsibleError("All sources must define src=path")
+
+        return sources_map
+
 
     def run(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
-        ''' handler for nginx_config operations '''
+        (
+        sources_complex_args_list,
+        passthru_complex_args_map
+        ) = self._partition_options(complex_args)
 
-        # extract passthrough arguments from module_args
+        sources_complex_args_map = self._make_sources_map(sources_complex_args_list)
 
-        # construct remote filesystem path
+        (
+        sources_module_args_hash_list,
+        passthru_module_args_hash
+        ) = self._partition_options(utils.parse_kv(module_args))
 
-        # if path doesn't start with /, assume that it is relative to the
-        # remote temporary directory
+        sources_module_args_map = self._make_sources_map(sources_module_args_hash_list)
 
-        # get path to local utils library
-        utils_src = os.path.join(self.runner.basedir, 'library', 'utils')
+        sources_options_map = utils.merge_hash(sources_complex_args_map, sources_module_args_map)
+        passthru_options_map = utils.merge_hash(passthru_complex_args_map, passthru_module_args_hash)
 
-        # ensure temporary directory exists on target
-        #if "tmp" not in tmp:
-        if tmp is None:
-            tmp= self.runner._make_tmp_path(conn)
+        # iterate over 'copy' files
+        for src, options in sources_options_map.iteritems():
+            # construct remote filesystem path
+            wanted_dest = options.get('dest', None)
 
-        # upload library files to temporary directory using the 'copy' module
-        copy_module_args = utils.merge_module_args('', dict(
-            src=utils_src,
-            dest=tmp,
-            original_basename=os.path.basename(utils_src),
-            follow=True,
-        ))
+            if wanted_dest is None:
+                if tmp is None:
+                    tmp = self.runner._make_tmp_path(conn)
+                options['dest'] = tmp
+            # interpret relative paths as starting with the remote tmp
+            # directory
+            elif not wanted_dest.startswith('/'):
+                if tmp is None:
+                    tmp = self.runner._make_tmp_path(conn)
+                options['dest'] = os.path.join(tmp, wanted_dest)
 
-        res = None
-        if self.runner.noop_on_check(inject):
-            return ReturnData(conn=conn, comm_ok=True, result=dict(skipped=True))
-        else:
-            # Instantiate the action_plugin for the 'copy' module
-            copy_handler = utils.plugins.action_loader.get('copy', self.runner)
+            copy_module_args = utils.serialize_args(sources_module_args_map.get(src, {}))
+            copy_complex_args = sources_complex_args_map.get(src, None)
 
-            # FIXME handle complex_args -- some of that stuff should probably
-            # be passed through to 'copy' (or maybe not?)
-            res = copy_handler.run(conn, tmp, 'copy', copy_module_args,
-                                   inject=inject)
+            # Copy source to destination.
+            #
+            # XXX because the 'copy' action_plugin doesn't pass through
+            # persist_files or delete_remote_tmp, we need to make a temporary
+            # adjustment to C.DEFAULT_KEEP_REMOTE_FILES.  The 'as' clause is
+            # necessary in order to affect C.DEFAULT_KEEP_REMOTE_FILES in the
+            # scope of ansible.runner.
+            res = None
+            with tmp_keep_remote_files(True) as C.DEFAULT_KEEP_REMOTE_FILES:
+                res = self._copy(conn, tmp, 'copy', copy_module_args, inject,
+                                 complex_args=copy_complex_args)
 
             # Fail here if libs weren't copied over correctly
             if res is not None and not res.is_successful():
                 return res
 
+        for passthru_name, passthru_options in passthru_options_map.iteritems():
+            passthru_complex_args = passthru_complex_args_map.get(passthru_name, None)
+            passthru_module_args = utils.serialize_args(passthru_module_args_hash)
 
-        return self.runner._execute_module(conn, tmp, 'nginx_config',
-                                           module_args, inject=inject,
-                                           complex_args=complex_args)
+            # Handle things like 'command: do_something'
+            if not isinstance(passthru_complex_args, dict):
+                if isinstance(passthru_complex_args, basestring):
+                    passthru_module_args = passthru_complex_args
+                passthru_complex_args = None
+
+            # Instantiate the action_plugin for the wanted module
+            passthru_handler = utils.plugins.action_loader.get(passthru_name, self.runner)
+
+            if passthru_handler:
+                return passthru_handler.run(conn, tmp, passthru_name,
+                                            passthru_module_args, inject,
+                                            complex_args=passthru_complex_args,
+                                            **kwargs)
+            else:
+                return self.runner._execute_module(conn, tmp, passthru_name,
+                                                   passthru_module_args,
+                                                   inject=inject,
+                                                   complex_args=passthru_complex_args,
+                                                   **kwargs)
+
